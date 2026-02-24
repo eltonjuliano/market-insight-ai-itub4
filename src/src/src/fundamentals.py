e64c9138a9d719c17e0fa5c5775adcd5d563cfa0
eltonjuliano <133607920+eltonjuliano@users.noreply.github.com>
2026-02-24 11:04:15 -0300

    Add get_fundamentals function to return financial data

---
+++ src/src/src/fundamentals.py
@@ -0,0 +1,8 @@
+def get_fundamentals():
+    return {
+        "P_L": 8.5,
+        "ROE": "18%",
+        "Dividend_Yield": "6.2%",
+        "Net_Margin": "21%",
+        "Debt_Level": "Controlled"
+    }
